4e08c2777c33b54b166b8381825699645864ca77
Bishal Chansi <bishal.chansi@office.mypay.com.np>
2026-01-09 14:11:29 +0545

    toggle functionality for theme mode and header

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -7,7 +7,7 @@ const AppLayout = () => {
       <main className="min-h-screen container mx-auto px-4 py-8">
         <Outlet />
       </main>
-      <footer className="border-t backdrop-blur py-12 supports-[backdrop-filter]: bg-background/60">
+      <footer className="border-t backdrop-blur py-12 supports-backdrop-filter:bg-background/60">
         <div className="container mx-auto px-4 text-center ">
           <p>Hello i am bishal chansi</p>
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,40 @@
+import { useTheme } from "@/context/ThemeProvider";
+import { Moon, Sun } from "lucide-react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  return <header>Header</header>;
+  const { theme, setTheme } = useTheme();
+  const isDark = theme === "dark";
+  return (
+    <header className="sticky top-0 z-50 w-full backdrop-blur bg-background/95 py-2 border-b supports-backdrop-filter:bg-background/60">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+        <NavLink to="/">
+          <img
+            src={isDark ? "/logo.png" : "/logo2.png"}
+            alt="climate logo"
+            className="h-14"
+          />
+        </NavLink>
+        <div>
+          {/* search */}
+          <div
+            onClick={() => {
+              setTheme(isDark ? "light" : "dark");
+            }}
+            className={`cursor-pointer ${
+              isDark ? "rotate-180" : "rotate-0"
+            } transition-all duration-500`}
+          >
+            {isDark ? (
+              <Sun className="text-yellow-500" />
+            ) : (
+              <Moon className="text-sky-500" />
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
